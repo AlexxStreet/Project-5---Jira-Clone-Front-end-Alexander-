8de220ae8b94957bb3a154f9ab40696f19fb778a
AlexxStreet <alexander.soloviev009@gmail.comb>
2023-07-20 20:38:30 +0300

    My commets about the commit

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -12,21 +12,28 @@ describe('Issue details editing', () => {
     });
   });
   
+  const iconTrash = '[data-testid="icon:trash"]'
+  const modalConfirm = '[data-testid="modal:confirm"]'
+  const iconClose = '[data-testid="icon:close"]'
+  const boardlistBacklog = '[data-testid="board-list:backlog'
+  const listIssue = '[data-testid="list-issue"]'
+  const boardlistInprogress = '[data-testid="board-list:inprogress"]'
+
  // Test 1: Create a new test case for deleting issue
 
   it('Should delete issue and assert, that it is not displayed on the Jira board anymore', () => {
     //Delete issue (click delete button)
-    cy.get('[data-testid="icon:trash"]').click()
+    cy.get(iconTrash).click()
     //Confirm deletion
-    cy.get('[data-testid="modal:confirm"]').contains('Delete issue').click()
+    cy.get(modalConfirm).contains('Delete issue').click()
     //Assert, that deletion confirmation dialogue is not visible.
-    cy.get('[data-testid="modal:confirm"]').should('not.exist')
+    cy.get(modalConfirm).should('not.exist')
     //Assert, that issue is deleted and not displayed on the Jira board anymore.
     cy.reload()
-    cy.get('[data-testid="board-list:inprogress"]').contains('This is an issue of type: Task.').should('not.exist')
+    cy.get(boardlistInprogress).contains('This is an issue of type: Task.').should('not.exist')
     //Assert than list with name Backlog is visible with only 3 tasks
-    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-      cy.get('[data-testid="list-issue"]')
+    cy.get(boardlistBacklog).should('be.visible').and('have.length', '1').within(() => {
+      cy.get(listIssue)
           .should('have.length', '3')
   });
 });
@@ -35,18 +42,18 @@ describe('Issue details editing', () => {
 
 it('Should cancel deleting issue and and still be displayed on the Jira board', () => {
   //Click delete issue button
-  cy.get('[data-testid="icon:trash"]').click()
+  cy.get(iconTrash).click()
   //Cancel the deletion in the confirmation pop-up.
-  cy.get('[data-testid="modal:confirm"]').contains('Cancel').click()
+  cy.get(modalConfirm).contains('Cancel').click()
   //Assert, that deletion confirmation dialogue is not visible.
-  cy.get('[data-testid="modal:confirm"]').should('not.exist')
+  cy.get(modalConfirm).should('not.exist')
   //Assert, that issue modal is closed
-  cy.get('[data-testid="icon:close"]').first().click()
+  cy.get(iconClose).first().click()
   //Assert, that issue is not deleted and still displayed on the Jira board.
   cy.reload()
-  cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+  cy.get(boardlistBacklog).should('be.visible').and('have.length', '1').within(() => {
     //Assert that this list contains 4 issues and first element with tag p has specified text
-    cy.get('[data-testid="list-issue"]')
+    cy.get(listIssue)
         .should('have.length', '4')
     });
   }); 

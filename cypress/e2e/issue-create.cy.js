a3a888d6079788a8bb079375bf48b36e41f03952
AlexxStreet <alexander.soloviev009@gmail.comb>
2023-08-06 13:48:48 +0300

    My commets about the commit

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -54,6 +55,7 @@ describe('Issue create', () => {
       //Assert that correct avatar and type icon are visible
       cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
       cy.get('[data-testid="icon:story"]').should('be.visible');
+
     });
   });
 
@@ -178,6 +180,35 @@ it('Should create an issue and validate it successfully', () => {
 
       //Assert that correct error message is visible
       cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
+    });
+  });
+
+  it.only('Should verifies that application is removing unnecessary spaces on the board view', () => {
+    const title = 'Hello     World';
+    let titleTrimmed = title.trim();
+
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+      
+
+      cy.get('[data-testid="select:type"]').click();
+      cy.get('.ql-editor').type('Here should be description');
+      cy.get('input[name="title"]').type(title);
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+      cy.get('button[type="submit"]').click();
+
+      cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+      cy.contains('Issue has been successfully created.').should('be.visible');
+  
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+      cy.reload();
+      cy.contains('Issue has been successfully created.').should('not.exist');
+
+      cy.get('[data-testid="list-issue"]').first().should('be.visible').and('contain', titleTrimmed)
     });
   });
 });
